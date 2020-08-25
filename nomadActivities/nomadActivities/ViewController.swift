8e30fc11a18336cd86b2fc793e82cc3febee84ce
ed1868 <ruizeduardo21@gmail.com>
2020-08-25 17:55:38 -0400

    nomad activites set up and scroll view complete

---
+++ nomadActivities/nomadActivities/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nomadActivities
+//
+//  Created by Eddie Ruiz on 8/25/20.
+//  Copyright © 2020 AI Nomads. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
